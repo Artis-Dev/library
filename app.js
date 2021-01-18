3709546efecf0a5de8f291f7219a8381b7acdcbc
Artis-Dev <admin@moonart.lt>
2021-01-18 19:50:39 +0200

    Add a function to create and add book's object to array

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-console.log('Hello World');
+const myLibrary = [];
+
+function Book() {
+}
+
+Book.prototype.info = function info() {
+  return `'${this.title}' by ${this.author}, ${this.pages} pages, status: ${this.read}`;
+};
+
+function addBookToLibrary(title, author, pages, read) {
+  const newBook = Object.create(Book.prototype);
+  newBook.title = title;
+  newBook.author = author;
+  newBook.pages = pages;
+  newBook.read = read;
+  myLibrary.push(newBook);
+  return `Book '${newBook.title}' has been added to the library.`;
+}
+
+addBookToLibrary('The Last Wish', 'Andrzej Sapkowski', '288', 'READ');
+addBookToLibrary('Sword of Destiny', 'Andrzej Sapkowski', '384', 'READ');
+addBookToLibrary('Blood of Elves', 'Andrzej Sapkowski', '320', 'NOT READ');
