34620383142c6ab3829fe48ab2864e2b00de7313
Artis-Dev <admin@moonart.lt>
2021-02-05 18:50:56 +0200

    Add 'add books' function to modal box, form validation, form errors

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const mainContainer = document.getElementById('books-container');
-
-const myLibrary = [];
+const modal = document.getElementById('new-book-modal');
 
 function Book() {}
 
+const myLibrary = [];
+
 // eslint-disable-next-line func-names
 Book.prototype.info = function () {
-  return `'${this.title}' by ${this.author}, ${this.pages} pages, status: ${this.read}`;
+  return `'${this.title}' by ${this.author}, ${this.pages} pages, status: ${this.status}`;
 };
 
 function showBooks() {
@@ -15,6 +16,8 @@ function showBooks() {
     // Create book card
     const divCard = document.createElement('div');
     divCard.classList.add('card');
+    // WIP
+    divCard.setAttribute('data-index', i);
     mainContainer.appendChild(divCard);
     // Create delete icon
     const spanDelete = document.createElement('span');
@@ -57,7 +60,7 @@ function showBooks() {
     spanStatus.classList.add('bold');
     spanStatus.textContent = 'Status: ';
     pStatus.appendChild(spanStatus);
-    const textStatus = document.createTextNode((myLibrary[i].read) ? 'Done' : 'In progress');
+    const textStatus = document.createTextNode((myLibrary[i].status) ? 'Done' : 'In progress');
     pStatus.appendChild(textStatus);
     const iChangeStatus = document.createElement('i');
     iChangeStatus.classList.add('fas', 'fa-sync-alt', 'change-status');
@@ -71,27 +74,60 @@ function showBooks() {
   const iAddBook = document.createElement('i');
   iAddBook.classList.add('fas', 'fa-plus', 'add-book-modal');
   divCard.appendChild(iAddBook);
+  // Add event listener for modal popup
+  const addBookButton = document.querySelector('.add-book-modal');
+  addBookButton.addEventListener('click', () => {
+    modal.style.display = 'block';
+  });
 }
 
-function addBookToLibrary(title, author, pages, read) {
+function addBookToLibrary(title, author, pages, status) {
   const newBook = Object.create(Book.prototype);
   newBook.title = title;
   newBook.author = author;
   newBook.pages = pages;
-  newBook.read = read;
+  newBook.status = status;
   myLibrary.push(newBook);
   showBooks();
   return `Book '${newBook.title}' has been added to the library.`;
 }
 
+function formValidation(event) {
+  const form = document.getElementById('add-book-form');
+  const titleError = document.querySelector('.title-error');
+  const authorError = document.querySelector('.author-error');
+  const pagesError = document.querySelector('.pages-error');
+  const bookTitle = document.forms['add-book-form']['book-title'].value;
+  const bookAuthor = document.forms['add-book-form']['book-author'].value;
+  const bookPages = document.forms['add-book-form']['book-pages'].value;
+  const bookStatus = document.forms['add-book-form']['book-status'].checked;
+  event.preventDefault();
+  if (bookTitle !== '' && bookAuthor !== '' && +bookPages > 0 && +bookPages < 10000) {
+    addBookToLibrary(bookTitle, bookAuthor, bookPages, bookStatus);
+    modal.style.display = 'none';
+    form.reset();
+  }
+  if (bookTitle === '') {
+    titleError.style.display = 'block';
+  } else if (bookTitle !== '') {
+    titleError.style.display = 'none';
+  }
+  if (bookAuthor === '') {
+    authorError.style.display = 'block';
+  } else if (bookTitle !== '') {
+    authorError.style.display = 'none';
+  }
+  if (bookPages === '' || +bookPages > 0 || +bookPages < 10000) {
+    pagesError.style.display = 'block';
+  } else if (bookTitle !== '') {
+    pagesError.style.display = 'none';
+  }
+}
+
 function modalListeners() {
-  const addBookButton = document.querySelector('.add-book-modal');
-  const modal = document.getElementById('new-book-modal');
+  const addBook = document.querySelector('#form-add-book');
   const closeButtons = document.getElementsByClassName('close');
 
-  addBookButton.addEventListener('click', () => {
-    modal.style.display = 'block';
-  });
   Array.from(closeButtons).forEach((button) => {
     button.addEventListener('click', () => {
       modal.style.display = 'none';
@@ -106,6 +142,9 @@ function modalListeners() {
     if (event.key === 'Escape') {
       modal.style.display = 'none';
     }
+  });
+  addBook.addEventListener('click', (event) => {
+    formValidation(event);
   });
 }
 
