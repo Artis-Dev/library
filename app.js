5a8280d900bd5616bb884ec9d00ffe33e4f05688
Artis-Dev <admin@moonart.lt>
2021-02-09 18:01:00 +0200

    Add 'remove book' function, fix bugs, cleanup

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const mainContainer = document.getElementById('books-container');
-const modal = document.getElementById('new-book-modal');
+const mainContainer = document.querySelector('#books-container');
+const modal = document.querySelector('#new-book-modal');
 
 function Book() {}
 
@@ -16,16 +16,12 @@ function showBooks() {
     // Create book card
     const divCard = document.createElement('div');
     divCard.classList.add('card');
-    // WIP
     divCard.setAttribute('data-index', i);
     mainContainer.appendChild(divCard);
     // Create delete icon
-    const spanDelete = document.createElement('span');
-    spanDelete.classList.add('delete');
-    divCard.appendChild(spanDelete);
     const iDelete = document.createElement('i');
-    iDelete.classList.add('fas', 'fa-trash-alt');
-    spanDelete.appendChild(iDelete);
+    iDelete.classList.add('delete', 'fas', 'fa-trash-alt');
+    divCard.appendChild(iDelete);
     // Create book title
     const h2Title = document.createElement('h2');
     h2Title.textContent = myLibrary[i].title;
@@ -74,11 +70,6 @@ function showBooks() {
   const iAddBook = document.createElement('i');
   iAddBook.classList.add('fas', 'fa-plus', 'add-book-modal');
   divCard.appendChild(iAddBook);
-  // Add event listener for modal popup
-  const addBookButton = document.querySelector('.add-book-modal');
-  addBookButton.addEventListener('click', () => {
-    modal.style.display = 'block';
-  });
 }
 
 function addBookToLibrary(title, author, pages, status) {
@@ -92,8 +83,16 @@ function addBookToLibrary(title, author, pages, status) {
   return `Book '${newBook.title}' has been added to the library.`;
 }
 
+function removeBookFromLibrary(event) {
+  if (event.target.className === 'delete fas fa-trash-alt') {
+    const index = event.target.parentElement.getAttribute('data-index');
+    myLibrary.splice(index, 1);
+  }
+  showBooks();
+}
+
 function formValidation(event) {
-  const form = document.getElementById('add-book-form');
+  const form = document.querySelector('#add-book-form');
   const titleError = document.querySelector('.title-error');
   const authorError = document.querySelector('.author-error');
   const pagesError = document.querySelector('.pages-error');
@@ -109,42 +108,52 @@ function formValidation(event) {
   }
   if (bookTitle === '') {
     titleError.style.display = 'block';
-  } else if (bookTitle !== '') {
+  } else {
     titleError.style.display = 'none';
   }
   if (bookAuthor === '') {
     authorError.style.display = 'block';
-  } else if (bookTitle !== '') {
+  } else {
     authorError.style.display = 'none';
   }
-  if (bookPages === '' || +bookPages > 0 || +bookPages < 10000) {
+  if (bookPages === '' || +bookPages < 0 || +bookPages > 10000) {
     pagesError.style.display = 'block';
-  } else if (bookTitle !== '') {
+  } else {
     pagesError.style.display = 'none';
   }
 }
 
 function modalListeners() {
   const addBook = document.querySelector('#form-add-book');
-  const closeButtons = document.getElementsByClassName('close');
+  const closeButtons = document.querySelectorAll('.close');
 
+  // Open/close modal
+  document.addEventListener('click', (event) => {
+    if (event.target.className === 'fas fa-plus add-book-modal') {
+      modal.style.display = 'block';
+    } else if (event.target === modal) {
+      modal.style.display = 'none';
+    }
+  });
+  // Close modal with buttons
   Array.from(closeButtons).forEach((button) => {
     button.addEventListener('click', () => {
       modal.style.display = 'none';
     });
   });
-  window.addEventListener('click', (event) => {
-    if (event.target === modal) {
-      modal.style.display = 'none';
-    }
-  });
+  // Close modal with escape key
   document.addEventListener('keyup', (event) => {
     if (event.key === 'Escape') {
       modal.style.display = 'none';
     }
   });
+  // Add book button
   addBook.addEventListener('click', (event) => {
     formValidation(event);
+  });
+  // Remove book button
+  document.addEventListener('click', (event) => {
+    removeBookFromLibrary(event);
   });
 }
 
@@ -154,3 +163,4 @@ addBookToLibrary('Blood of Elves', 'Andrzej Sapkowski', '320', false);
 
 showBooks();
 modalListeners();
+removeBookFromLibrary();
